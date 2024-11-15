b5b439c3993a958e564fd454620d014e8d74db57
CryptoSOL123 <pradyun4050@gmail.com>
2024-11-14 20:21:47 -0800

    Made the contact page

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,12 @@
+import { NavHeader } from "@/components/NavHeader";
+import  Contact  from "@/components/Contact";
+
+
+export default function ContactPage() {
+    return (
+       <>
+        <NavHeader />
+         <Contact />
+        </>
+    ) 
+}
