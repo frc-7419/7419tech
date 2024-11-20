2ae7bca6f4851a531653a82e135bfe7edd3f77ad
Khoi <khoi304857@gmail.com>
2024-11-19 18:05:06 -0800

    Page.tsx fix/restored outreach.tsx
    Originally put code under apps in page.tsx but then I realized I was wrong. Code should stay under components and just copy over the page.tsx code from the other pages to create a new outreach page.

---
+++ src/app/outreach/page.tsx
@@ -0,0 +1,13 @@
+import { NavHeader } from "@/components/NavHeader";
+import  Outreach  from "@/components/Outreach";
+
+
+
+export default function OutreachPage() {
+    return (
+       <>
+        <NavHeader />
+        <Outreach />
+        </>
+    ) 
+}
