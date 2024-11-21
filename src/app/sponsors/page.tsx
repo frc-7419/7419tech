a9b5d8689580393550259efef540870f49ec24a0
AnmingZhang <anmingz85@gmail.com>
2024-11-21 11:24:22 -0800

    Sponsors page (needs update)
    added sponsors from old website (probably needs to be updated - check with business)

---
+++ src/app/sponsors/page.tsx
@@ -0,0 +1,8 @@
+import { NavHeader } from "@/components/NavHeader";
+import Sponsors from "@/components/Sponsors";
+
+export default function Robot() {
+    return (
+        <><NavHeader /><Sponsors /></>
+    ) 
+}
